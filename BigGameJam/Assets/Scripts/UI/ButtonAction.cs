1a3a1dcfa68d448f20d59e5a3c590eff686241b5
Thomas Stassin <thomas.stassin@gsmproductions.com>
2015-04-11 03:57:00 +0200

    main menu

---
+++ BigGameJam/Assets/Scripts/UI/ButtonAction.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonAction : MonoBehaviour {
+
+    public void ChangeScene(string levelName) {
+         Application.LoadLevel(levelName);
+    }
+
+    public void ChangeScene(int levelId) {
+         Application.LoadLevel(levelId);
+    }
+}
