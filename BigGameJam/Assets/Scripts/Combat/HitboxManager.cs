d7a7749ad2f7a74eb2413a066234c254c86a11c8
Bastien Gorissen <bastien.gorissen@gsmproductions.com>
2015-04-11 04:02:30 +0200

    Added combat test scene and hitbox script

---
+++ BigGameJam/Assets/Scripts/Combat/HitboxManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitboxManager : MonoBehaviour {
+
+    public PolygonCollider2D frame2;
+    public PolygonCollider2D frame3;
+
+    private PolygonCollider2D[] colliders;
+
+    private PolygonCollider2D localCollider;
+
+    public enum hitBoxes {
+        frame2Box,
+        frame3Box,
+        clear // special case to remove all boxes
+    }
+    
+    void Start() {
+        // Set up an array so our script can more easily set up the hit boxes
+        colliders = new PolygonCollider2D[]{frame2, frame3};
+
+        // Create a polygon collider
+        localCollider = gameObject.AddComponent<PolygonCollider2D>();
+        localCollider.isTrigger = true; // Set as a trigger so it doesn't collide with our environment
+        localCollider.pathCount = 0; // Clear auto-generated polygons
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        Debug.Log("Collider hit something!");
+    }
+
+    public void setHitBox(hitBoxes val) {
+        if(val != hitBoxes.clear) {
+            localCollider.SetPath(0, colliders[(int)val].GetPath(0));
+            return;
+        }
+        localCollider.pathCount = 0;
+    }
+}
