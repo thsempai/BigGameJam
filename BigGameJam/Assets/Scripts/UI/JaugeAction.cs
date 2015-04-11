f0e0baf66bafad630ebe5fda5002a52e6ecfbf04
Thomas Stassin <thomas.stassin@gsmproductions.com>
2015-04-11 15:24:57 +0200

    jauuuuuge

---
+++ BigGameJam/Assets/Scripts/UI/JaugeAction.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JaugeAction : MonoBehaviour {
+
+    public void UpdateJauge(float pc) {
+    GetComponent<UnityEngine.UI.Slider>().value = pc;
+    }
+}
+
